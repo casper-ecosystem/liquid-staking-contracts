d27bb341304c08a4ac8a796e3876a9b4439cddd6
Kuba Płaskonka <kuba@odra.dev>
2025-03-27 13:03:18 +0100

    Opts

--- liquid-staking-cli/bin/main.rs
+++ liquid-staking-cli/bin/main.rs
@@ -1,9 +1,15 @@
 use liquid_staking_contracts::token::{StakedCSPR, StakedCSPRInitArgs, MIN_STAKE};
-use odra::{
-    casper_types::U512,
-    host::{Deployer, HostEnv},
-};
+use odra::{casper_types::{bytesrepr::FromBytes, PublicKey, U512}, host::{Deployer, HostEnv}};
 use odra_cli::OdraCli;
+
+pub fn validator() -> PublicKey {
+    let bytes = "0106ca7c39cd272dbf21a86eeb3b36b7c26e2e9b94af64292419f7862936bca2ca";
+    let bytes = hex::decode(bytes).expect("Failed to decode hex");
+    let (pk, _) = PublicKey::from_bytes(&bytes).unwrap();
+    pk
+}
+
+const ONE_HOUR: u64 = 60 * 60 * 1000;
 
 pub struct DeployScript;
 impl odra_cli::deploy::DeployScript for DeployScript {
@@ -12,12 +18,12 @@ impl odra_cli::deploy::DeployScript for DeployScript {
         env: &HostEnv,
         container: &mut odra_cli::DeployedContractsContainer,
     ) -> Result<(), odra_cli::deploy::Error> {
-        env.set_gas(250_000_000_000);
+        env.set_gas(500_000_000_000);
         let token = StakedCSPR::try_deploy(
             env,
             StakedCSPRInitArgs {
-                validator_address: env.get_validator(0),
-                claim_time: env.auction_delay() * 8,
+                validator_address: validator(),
+                claim_time: 2 * ONE_HOUR * 8,
                 fee_percentage: 1000.into(),
                 min_stake: U512::from(MIN_STAKE),
             },
